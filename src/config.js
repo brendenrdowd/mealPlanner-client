85be489491c28e68a3b11a9debe6de956456c0ed
Brenden Dowd <brenden.dowd@yahoo.com>
2020-04-02 19:57:29 -0700

    updated endpoint

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
-require('dotenv').config()
-
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://dry-brushlands-83819.herokuapp.com/api/' || 'http://localhost:8000/api',
   FOOD_API_KEY: 'fT7bfv7YGGmshOWpHRupMoc5eXzLp1mUWeEjsn9TtTgMefEoGe',
   TOKEN_KEY: 'mealplan-auth-token',
 }
